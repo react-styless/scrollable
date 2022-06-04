4dedf6fc2f0e7e5eb56a49e52a093134d15c1bd2
kyun <wls5563@gmail.com>
2022-06-04 15:08:59 +0900

    add examples

---
+++ examples/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Scrollable from "@react-styless/scrollable";
+
+function App() {
+  return (
+    <div className="App">
+      <Scrollable>123</Scrollable>
+    </div>
+  );
+}
+
+export default App;
